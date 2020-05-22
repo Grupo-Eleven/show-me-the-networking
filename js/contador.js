aa35af9ab3d5d8aa32119f579574882f0fe09ff9
Mirelle Silva <mirellepollyane@gmail.com>
2020-05-22 14:11:45 +0100

    Folder organization

--- js/contador.js
+++ js/contador.js
@@ -3,7 +3,6 @@ var dias, horas, minutos, segundos;
 var regressiva = document.getElementById("regressiva");
 
 setInterval(function () {
-
     var current_date = new Date().getTime();
     var segundos_f = (target_date - current_date) / 1000;
 
@@ -21,5 +20,4 @@ setInterval(function () {
     document.getElementById('minuto').innerHTML = minutos;
     document.getElementById('segundo').innerHTML = segundos;
   
-
 }, 1000);
